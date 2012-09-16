1b066be5760b9defe0d00e1ed8c2758a558c54ff
Mianwo <shengcer@gmail.com>
2012-09-16 14:20:51 -0400

    initial checkin
    The real dist is an exec file generated by pyinstaller

---
+++ Checker.py
@@ -0,0 +1,245 @@
+'''
+Created on Sep 14, 2012
+
+@author: ShengC
+'''
+
+import cookielib
+import urllib2
+import urllib
+from HTMLParser import HTMLParser
+import re
+import json
+from time import strptime
+import os
+
+import pylab
+
+class MyHTMLParser(HTMLParser):
+    """
+    For the sake of efficiency, this is not thread safe! 
+    """
+    keyPattern = re.compile("^Your Current Case Status for Form (.*)$")
+    timePattern = re.compile("^On(.*?) we.*$")
+    
+    def __init__(self):
+        self._caseTypeFound=False
+        self._caseStatusFound=False
+        self._caseTimeFound=False
+        self._stat = {}.fromkeys([Checker.type_field, Checker.status_field, Checker.time_field])
+        HTMLParser.__init__(self)
+
+    def handle_starttag(self, tag, attrs):
+        attrs = dict(attrs)
+        if attrs.has_key('id') and attrs['id'] == 'caseStatus':
+            self._caseTypeFound=True
+        elif tag == 'img' and attrs.has_key('src') and 'bucket-on' in attrs['src']:
+            self._caseStatusFound = True
+        elif attrs.has_key('class') and attrs['class'] == 'caseStatus':
+            self._caseTimeFound=True
+            
+    def handle_endtag(self, tag):
+        pass
+    def handle_data(self, data):
+        if self._caseTypeFound and data.strip():
+            mat = MyHTMLParser.keyPattern.match(data.strip())
+            if mat is not None:
+                self._stat[Checker.type_field] = mat.groups()[0].split(',')[0].strip()
+            self._caseTypeFound = False
+        elif self._caseStatusFound and data.strip():
+            self._stat[Checker.status_field] = data.strip()
+            self._caseStatusFound = False
+        elif self._caseTimeFound:
+            mat = MyHTMLParser.timePattern.match(data.strip())
+            if mat is not None:
+                self._stat[Checker.time_field] = mat.groups()[0].rsplit(',', 1)[0].strip()
+            self._caseTimeFound = False
+    
+    def __reset(self):
+        self._caseTypeFound=False
+        self._caseStatusFound=False
+        self._caseTimeFound=False
+        
+    def get(self):
+        self.__reset()
+        return self._stat.copy()
+ 
+    def feed(self, data):
+        self._stat = {}.fromkeys([Checker.type_field, Checker.status_field, Checker.time_field])
+        HTMLParser.feed(self, data)
+
+class Checker(object):
+    
+    base_url = "https://egov.uscis.gov/cris/Dashboard/CaseStatus.do"
+    id_pattern = re.compile("^([a-zA-Z]*)([0-9]*)$")
+    
+    type_field = 'type'
+    status_field = 'status'
+    time_field = 'time'
+    
+    def __init__(self):
+        self.parser = MyHTMLParser()
+    
+    @staticmethod
+    def response(status_id):
+        assert type(status_id) is str or type(status_id) is unicode, "status_id needs to be a string"
+
+        cj = cookielib.CookieJar()
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+        urllib2.install_opener(opener)
+        
+        values = dict(appReceiptNum=status_id)
+        data = urllib.urlencode(values)
+        
+        req = urllib2.Request(Checker.base_url, data)
+        res = opener.open(req) 
+        ret = res.read()
+        
+        opener.close()
+        return ret
+
+    def validateCase(self, status_id):
+        self.parser.feed(Checker.response(status_id))
+        return reduce(lambda x, y: x and y, [s is not None for s in self.parser.get().values()])
+
+    def taskManager(self, petition_id, query_range, types, log, progress_bar=None, save2file=False):
+        assert type(query_range) is int and len(types) > 0
+        
+        def join(stats, stat):
+            if not stats.has_key(stat[Checker.type_field]): stats[stat[Checker.type_field]] = {}
+            if not stats[stat[Checker.type_field]].has_key(stat[Checker.status_field]): stats[stat[Checker.type_field]][stat[Checker.status_field]] = {}
+            if not stats[stat[Checker.type_field]][stat[Checker.status_field]].has_key(stat[Checker.time_field]): stats[stat[Checker.type_field]][stat[Checker.status_field]][stat[Checker.time_field]] = 0
+            stats[stat[Checker.type_field]][stat[Checker.status_field]][stat[Checker.time_field]] += 1
+            return stats
+            
+#            if not stats[stat[Checker.type_field]].has_key(stat[Checker.time_field]): stats[stat[Checker.type_field]][stat[Checker.time_field]] = {} 
+#            if not stats[stat[Checker.type_field]][stat[Checker.time_field]].has_key(stat[Checker.status_field]): stats[stat[Checker.type_field]][stat[Checker.time_field]][stat[Checker.status_field]] = 0
+#            stats[stat[Checker.type_field]][stat[Checker.time_field]][stat[Checker.status_field]] += 1
+#            return stats
+        
+        mats = Checker.id_pattern.match(petition_id)
+        string, number = mats.groups()
+        number = int(number)
+        count1 = 0
+        consec_fail = 0
+        incrementor = 0
+        
+        stats = []
+        log.info("Initialization Done! Start lower range search...")
+        while count1 < query_range:
+            status_id = "%s%d" % (string, (number - incrementor))
+            self.parser.feed(Checker.response(status_id))
+            stat = self.parser.get()
+            if stat[self.type_field] in types:
+                if reduce(lambda x, y: x and y, [s is not None for s in stat.values()]):
+                    log.info("processing %s... Accepted type %s" % (status_id, stat[self.type_field]))
+                    stats.append(stat)        
+                    count1 += 1
+                    if progress_bar is not None: progress_bar.Update(count1)
+                    consec_fail = 0
+                else:
+                    log.info("processing %s... Rejected" % status_id) 
+                    consec_fail += 1
+            else: log.info("processing %s... Rejected type %s" % (status_id, stat[self.type_field])) 
+            
+            if consec_fail > 3: break
+            incrementor += 1;
+
+        log.info("Lower range search done. Start upper range search...")
+            
+        remains = query_range - count1
+        count2 = 0
+        consec_fail = 0
+        incrementor = 0
+        while count2 < query_range + remains:
+            status_id = "%s%d" % (string, (number + incrementor)) 
+            self.parser.feed(Checker.response(status_id))
+            stat = self.parser.get()
+            if stat[self.type_field] in types:                
+                if reduce(lambda x, y: x and y, [s is not None for s in stat.values()]):
+                    log.info("processing %s... Accepted %s" % (status_id, stat[self.type_field]))
+                    stats.append(stat)
+                    count2 += 1
+                    if progress_bar is not None: progress_bar.Update(count1 + count2)
+                    consec_fail = 0
+                else: 
+                    log.info("processing %s... Rejected" % status_id)
+                    consec_fail += 1
+            else: log.info("processing %s... Rejected type %s" % (status_id, stat[self.type_field]))
+            
+            if consec_fail > 3: break
+            incrementor += 1
+
+        log.info("Search done. Found %d cases fitting the desired types" % (count1 + count2))
+        print "Generating reports...."
+        print "=" * 20 + "\n\n\n"
+        
+        stats = reduce(lambda x, y: join(x, y), stats, {})
+        print stats
+        
+        report = json.dumps(stats, sort_keys=True, indent=4)
+        print report
+        if save2file:
+            with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'report.txt'), 'w') as fout:
+                fout.write(report)
+        
+        return stats
+
+class Plotter(object):
+
+    bins = range(1, 12 + 1)
+    
+    class ColorGenerator(object):
+        colors = ['crimson', 'burlywood', 'chartreuse', 'magenta', 'cyan']
+        
+        def __init__(self):
+            self.c = 0
+        
+        def get(self):
+            color = self.colors[self.c]
+            self.c = 0 if self.c == len(self.colors) - 1 else (self.c + 1)
+            return color
+    
+    def __init__(self):    
+        self.color_generator = Plotter.ColorGenerator()
+    
+    def plot(self, stats, types):
+        for p_type in stats.iterkeys():
+            d_data = {}
+            for p_stat in stats[p_type].iterkeys():
+                for p_time in stats[p_type][p_stat].iterkeys():
+                    t = strptime(p_time, "%B %d, %Y")
+                    y = t[0]
+                    m = t[1]
+                    
+                    k = '%s:%s' % (y, p_stat)
+                    if not d_data.has_key(k): d_data[k] = ([], self.color_generator.get())
+                    d_data[k][0].extend([m] * stats[p_type][p_stat][p_time]) 
+            
+#            for p_time in stats[p_type].iterkeys():
+#                t = strptime(p_time, "%B %d, %Y")
+#                y = t[0]
+#                m = t[1]
+#                
+#                for p_stat in stats[p_type][p_time]:
+#                    k = '%s:%s' % (y, p_stat)
+#                    if not d_data.has_key(k): d_data[k] = ([], self.color_generator.get())
+#                    d_data[k][0].extend([m] * stats[p_type][p_time][p_stat])
+            
+            sort_keys = sorted(d_data.keys())
+                
+            pylab.figure()
+            pylab.xlabel('month')
+            pylab.ylabel('numCases')
+            pylab.title(p_type)
+            pylab.hist([d_data[x][0] for x in sort_keys], Plotter.bins, histtype='barstacked', color=[d_data[x][-1] for x in sort_keys], label=sort_keys)
+            pylab.legend()  
+        pylab.show() 
+
+if __name__ == '__main__':
+    checker = Checker()
+    stats = checker.taskManager('SRC1280014743', 20, ['I140', 'I485'])
+    print stats
+    plotter = Plotter()
+    plotter.plot(stats, ['I140'])
+    
---
+++ Form.py
@@ -0,0 +1,169 @@
+'''
+Created on Sep 14, 2012
+
+@author: ShengC
+'''
+
+import wx
+from Checker import Checker, Plotter
+import logging
+import sys
+
+class WxLog(logging.Handler):
+    def __init__(self, ctrl):
+        logging.Handler.__init__(self)
+        self.ctrl = ctrl
+    def emit(self, record):
+        self.ctrl.AppendText(self.format(record)+"\n")
+
+class Frame(wx.Frame):
+    class RedirectText(object):
+        def __init__(self,aWxTextCtrl):
+            self.out=aWxTextCtrl
+     
+        def write(self,string):
+            self.out.WriteText(string)
+    
+    def __init__(self, app, title):
+        self.checker = Checker()
+        self.plotter= Plotter()
+        self.app = app
+        
+        wx.Frame.__init__(self, None, title=title, size=(400,400))
+        self.Bind(wx.EVT_CLOSE, self.OnClose)
+        
+        panel = wx.Panel(self)
+        panel.SetBackgroundColour('#ededed')
+        
+        box = wx.BoxSizer(wx.VERTICAL)
+        
+        m_text = wx.StaticText(panel, -1, "Immigration Status Checker")
+        m_text.SetFont(wx.Font(14, wx.SWISS, wx.NORMAL, wx.BOLD))
+        m_text.SetSize(m_text.GetBestSize())
+        box.Add(m_text, 0, wx.ALL, 10)
+        
+        id_box = wx.GridSizer(1, 2)
+        m_id_text = wx.StaticText(panel, -1, "petition no.")
+        id_box.Add(m_id_text, 0, wx.ALL, 10)
+        
+        self.m_id_textbox = wx.TextCtrl(panel)
+        id_box.Add(self.m_id_textbox, 0, wx.ALL, 10)
+        box.AddSizer(id_box)
+        
+        range_box = wx.GridSizer(1, 2)
+        
+        m_range_text = wx.StaticText(panel, -1, "range")
+        m_range_text.SetSize(m_range_text.GetBestVirtualSize())
+        range_box.Add(m_range_text, 0, wx.ALL, 10)
+        
+        self.m_range_textbox = wx.TextCtrl(panel, value="10")
+        range_box.Add(self.m_range_textbox, 0, wx.ALL, 10)
+
+        box.AddSizer(range_box)
+        
+        type_box = wx.BoxSizer(wx.HORIZONTAL)
+        self.m_I140_checkbox = wx.CheckBox(panel, -1, 'I140')
+        type_box.Add(self.m_I140_checkbox, 0, wx.ALL, 10)
+        self.m_I485_checkbox = wx.CheckBox(panel, -1, 'I485')
+        type_box.Add(self.m_I485_checkbox, 0, wx.ALL, 10)
+        
+        box.AddSizer(type_box)
+
+        decision_box = wx.BoxSizer(wx.HORIZONTAL)
+        self.m_submit = wx.Button(panel, wx.ID_APPLY, 'Submit')
+        self.m_submit.Bind(wx.EVT_BUTTON, self.OnSubmit)
+        decision_box.Add(self.m_submit, 0, wx.ALL, 20)
+
+        m_close = wx.Button(panel, wx.ID_CLOSE, "Close")
+        m_close.Bind(wx.EVT_BUTTON, self.OnClose)
+        decision_box.Add(m_close, 0, wx.ALL, 20)
+        
+        self.m_clear = wx.Button(panel, wx.ID_CLEAR, 'Clear')
+        self.m_clear.Bind(wx.EVT_BUTTON, self.OnClear)
+        decision_box.Add(self.m_clear, 0, wx.ALL, 20)
+        
+        box.AddSizer(decision_box)
+        
+        self.m_report_to_file = wx.CheckBox(panel, -1, 'save to file?')
+        box.Add(self.m_report_to_file, 0, wx.RIGHT | wx.ALIGN_RIGHT, 10)
+                
+        self.log = wx.TextCtrl(panel, wx.ID_ANY, size=(300,100), style = wx.TE_MULTILINE|wx.TE_READONLY|wx.HSCROLL)
+        box.Add(self.log, 0, wx.EXPAND|wx.ALL, 10)
+        
+        self.logr = logging.getLogger('')
+        self.logr.setLevel(logging.INFO)
+        hdlr = WxLog(self.log)
+        hdlr.setFormatter(logging.Formatter('[%(levelname)s] %(asctime)-15s:  %(message)s'))
+        self.logr.addHandler(hdlr)
+
+        sys.stdout = Frame.RedirectText(self.log)
+        sys.stderr = Frame.RedirectText(self.log)
+        
+        panel.SetSizer(box)
+        panel.Layout()
+
+    def OnClear(self, event):
+        self.m_id_textbox.Clear()
+        self.m_range_textbox.Clear()
+        self.m_I140_checkbox.SetValue(False)
+        self.m_I485_checkbox.SetValue(False)
+        self.m_report_to_file.SetValue(False)
+        self.log.Clear()
+
+    def _validateFields(self):
+        if not self.m_range_textbox.Value.isdigit() or int(self.m_range_textbox.Value) <= 0 or int(self.m_range_textbox.Value) > 100:
+            dlg = wx.MessageDialog(self, "range needs to be an integer in (0, 100]", "Field Error", wx.OK|wx.ICON_ERROR)
+            dlg.ShowModal()
+            dlg.Destroy()
+            return False
+        elif not self.m_I140_checkbox.GetValue() and not self.m_I485_checkbox.GetValue():
+            dlg = wx.MessageDialog(self, "needs to select at least from I140 and I485", "Field Error", wx.OK|wx.ICON_ERROR)
+            dlg.ShowModal()
+            dlg.Destroy()
+            return False
+        elif not self.checker.validateCase(self.m_id_textbox.GetValue()):
+            dlg = wx.MessageDialog(self, "invalid status id", "Field Error", wx.OK|wx.ICON_ERROR)
+            dlg.ShowModal()
+            dlg.Destroy()
+            return False
+        else:
+            return True
+
+    def OnSubmit(self, event):
+        try:
+            self.m_submit.Disable()
+            self.m_range_textbox.Disable()
+            self.m_I140_checkbox.Disable()
+            self.m_I485_checkbox.Disable()
+            self.m_id_textbox.Disable()
+            
+            if not self._validateFields(): return
+            else:
+                t = []
+                if self.m_I140_checkbox.GetValue(): t.append('I140')
+                if self.m_I485_checkbox.GetValue(): t.append('I485')
+                
+                dialog = wx.ProgressDialog('progress bar', 'working...', int(self.m_range_textbox.GetValue()) * 2, self.m_range_textbox)
+                stats = self.checker.taskManager(self.m_id_textbox.GetValue(), 
+                                         int(self.m_range_textbox.GetValue()), 
+                                         t, 
+                                         self.logr,
+                                         dialog,
+                                         self.m_report_to_file.GetValue())
+                self.plotter.plot(stats, t)
+                dialog.Destroy()
+        finally:
+            wx.App.Yield(self.app)
+            self.m_id_textbox.Enable()
+            self.m_I485_checkbox.Enable()
+            self.m_I140_checkbox.Enable()
+            self.m_range_textbox.Enable()
+            self.m_submit.Enable()
+
+    def OnClose(self, event):
+        self.Destroy()
+
+app=wx.App(redirect=False)
+top = Frame(app, "Immigration Status Checker")
+top.Show()
+app.MainLoop()
